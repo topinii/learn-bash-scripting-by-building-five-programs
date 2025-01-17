1bc59a4f4d54ddae29a0dcd83daf97eecf2ebf57
topinii r <niiranentopi@gmail.com>
2025-01-17 10:45:30 +0000

    Save progress: 1220.1

--- fortune.sh
+++ fortune.sh
@@ -5,6 +5,7 @@ RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Don't count on it" "Ask again late
 N=$(( RANDOM % 6 ))
 GET_FORTUNE() {
   echo Ask a yes or no question:
+  read QUESTION
 }
 GET_FORTUNE
 echo ${RESPONSES[$N]}
