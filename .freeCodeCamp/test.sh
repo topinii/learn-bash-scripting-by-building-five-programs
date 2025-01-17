5d61defb31344982ab01efe0f2d12c5bb7367dec
topinii r <niiranentopi@gmail.com>
2025-01-17 10:26:11 +0000

    Save progress: 940.1

--- .freeCodeCamp/test.sh
+++ .freeCodeCamp/test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 #Bingo Number Generator
 echo -e "\n~~ Bingo Number Generator ~~\n"
-NUMBER=5
+NUMBER=$(( RANDOM % 75 + 1 ))
 echo $NUMBER
+TEXT="The next number is, "
+echo $TEXT
--- bingo.sh
+++ bingo.sh
@@ -3,3 +3,4 @@
 echo -e "\n~~ Bingo Number Generator ~~\n"
 NUMBER=$(( RANDOM % 75 + 1 ))
 echo $NUMBER
+TEXT="The next number is, "
